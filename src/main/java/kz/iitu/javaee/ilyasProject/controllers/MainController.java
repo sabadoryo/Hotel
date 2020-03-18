db56eb1e2ad4e44f664160e59050380133e0e91b
sabadoryo <eabilbay@gmail.com>
2020-03-18 13:28:17 +0600

    ---test

--- src/main/java/kz/iitu/javaee/ilyasProject/controllers/MainController.java
+++ src/main/java/kz/iitu/javaee/ilyasProject/controllers/MainController.java
@@ -3,7 +3,7 @@ package kz.iitu.javaee.ilyasProject.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//asdasdasdasdasdasdasdasdasdasdas
+//asdasdasdasdasdasdasdasdasdasdass
 @Controller
 public class MainController {
 
